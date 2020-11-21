452fe5bb909a57eb767b2700c12589dd74939ee8
michael-mcmasters <jmcmasters411@gmail.com>
2020-11-20 20:14:47 -0500

    Clean up code

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,40 +1,42 @@
 package hbcu.stay.ready.algor;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Solution {
 
-    public Boolean harmlessRansomNote(String message , String magazineText){
-        String[] magazineArray = magazineText.split(" ");
-        Map<String, Integer> cutoutWords = new HashMap<>();
-        for (String word : magazineArray) {
-            if (cutoutWords.containsKey(word)) {
-                Integer timesInHashmap = cutoutWords.get(word);
-                timesInHashmap++;
-                cutoutWords.put(word, timesInHashmap);
-            } else {
-                cutoutWords.put(word, 1);
-            }
-        }
+    public Boolean harmlessRansomNote(String message, String magazineText) {
+        Map<String, Integer> numTimesWordsExist = getNumTimesWordsAppear(magazineText);
 
         String[] messageArr = message.split(" ");
         for (String word : messageArr) {
-            if (cutoutWords.containsKey(word)) {
-                if (cutoutWords.get(word) < 1) {
-                    return false;
-                } else {
-                    int timesInHashmap = cutoutWords.get(word);
-                    timesInHashmap--;
-                    cutoutWords.put(word, timesInHashmap);
-                }
+            if (!numTimesWordsExist.containsKey(word) || numTimesWordsExist.get(word) < 1)
+                return false;       // if word isn't in hashmap, or times it has appeared is less than 1, there aren't enough words to create message. Return false.
+
+            int numTimesWordExists = numTimesWordsExist.get(word);
+            numTimesWordExists--;
+            numTimesWordsExist.put(word, numTimesWordExists);
+        }
+
+        // If code reaches this point, there were enough words in magazineText to create message.
+        return true;
+    }
+
+    // String for every word. Integer for times it appears in array.
+    private Map<String, Integer> getNumTimesWordsAppear(String magazineText) {
+        Map<String, Integer> numTimesWordsAppear = new HashMap<>();
+
+        String[] magazineArray = magazineText.split(" ");
+        for (String word : magazineArray) {
+            if (numTimesWordsAppear.containsKey(word)) {
+                Integer numTimesWordAppears = numTimesWordsAppear.get(word);
+                numTimesWordAppears++;
+                numTimesWordsAppear.put(word, numTimesWordAppears);
             } else {
-                return false;
+                numTimesWordsAppear.put(word, 1);
             }
         }
 
-
-        return true;
+        return numTimesWordsAppear;
     }
 }
