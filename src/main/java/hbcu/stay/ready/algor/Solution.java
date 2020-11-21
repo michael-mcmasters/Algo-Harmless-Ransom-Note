c7baae174e53a86ae7764bf2b01804752ac954af
michael-mcmasters <jmcmasters411@gmail.com>
2020-11-20 19:57:16 -0500

    Complete problem and pass all tests

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,8 +1,40 @@
 package hbcu.stay.ready.algor;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Solution {
 
     public Boolean harmlessRansomNote(String message , String magazineText){
-        return null;
+        String[] magazineArray = magazineText.split(" ");
+        Map<String, Integer> cutoutWords = new HashMap<>();
+        for (String word : magazineArray) {
+            if (cutoutWords.containsKey(word)) {
+                Integer timesInHashmap = cutoutWords.get(word);
+                timesInHashmap++;
+                cutoutWords.put(word, timesInHashmap);
+            } else {
+                cutoutWords.put(word, 1);
+            }
+        }
+
+        String[] messageArr = message.split(" ");
+        for (String word : messageArr) {
+            if (cutoutWords.containsKey(word)) {
+                if (cutoutWords.get(word) < 1) {
+                    return false;
+                } else {
+                    int timesInHashmap = cutoutWords.get(word);
+                    timesInHashmap--;
+                    cutoutWords.put(word, timesInHashmap);
+                }
+            } else {
+                return false;
+            }
+        }
+
+
+        return true;
     }
 }
